d02c4f1e016955c4a87b9530f0e41aee29dd04a2
Philip Helgesen <55789033+PhilipHelgesen@users.noreply.github.com>
2021-11-25 11:46:35 +0100

    added a pipeline.yml for pipeline GitHub actions

--- src/test/java/com/pgr301/exam/AccountTest.java
+++ src/test/java/com/pgr301/exam/AccountTest.java
@@ -7,6 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest
@@ -15,12 +16,20 @@ public class AccountTest {
     @Test
     public void testGetAccount(){
         Account account = new Account();
-        assertEquals(account.getCurrency(), "NOK");
+        assertNotNull(account);
     }
 
     @Test
     public void testGetBalance(){
         Account account = new Account();
         assertEquals(account.getBalance(), BigDecimal.valueOf(0));
+    }
+
+    @Test
+    public void testChangeBalance(){
+        Account account = new Account();
+        BigDecimal newBalance = BigDecimal.valueOf(2000);
+        account.setBalance(newBalance);
+        assertEquals(account.getBalance(), newBalance);
     }
 }
